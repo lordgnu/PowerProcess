3654c2f413fd363ff0d66bde85b3bed571afab72
Don Bauer <lordgnu@me.com>
2013-04-01 14:20:02 -0500

    Added two basic exceptions
    Still needs expanding and more exceptions need to be added

---
+++ lib/PowerProcess/Exception/ProcessForkException.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace BauerBox\PowerProcess\Exception;
+
+/**
+ * Description of PowerProcessException
+ *
+ * @author Don Bauer <don.bauer@gorevco.com>
+ */
+class ProcessForkException extends \RuntimeException
+{
+    //put your code here
+}
